011fdb51f26e5345acf3ea900f575559c64545d6
Benjamin Zagorski <benj@trustsherpa.com>
2017-04-08 00:02:44 -0700

    Form is now cleared upon submission

--- script/note.js
+++ script/note.js
@@ -14,6 +14,7 @@ function saveNote() {
       $("#noteDisplay").html() + noteTime + ": " + noteText + "<br>"
     );
 
+    $("#noteInput").val("");
     $("#noteForm").addClass("invisible");
     jwplayer().play(true);
   }
